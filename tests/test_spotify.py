ce2902781ef2e5154f58d784d2fa31cd9c63d512
suraj dengale <suraj.dengale@salaryfinance.com>
2023-12-07 20:26:33 +0530

    added new test to login into spotify(in progress)

---
+++ tests/test_spotify.py
@@ -0,0 +1,22 @@
+# coding=utf-8
+import pytest
+from selenium.webdriver.common.by import By
+
+from pages.spotify_page import Spotify
+from tests.base_test import BaseTest
+from data.SpotifyLocators import SpotifyHomePageLocators
+
+
+class TestSpotify(BaseTest):
+
+    @pytest.fixture
+    def load_pages(self):
+        self.page = Spotify(self.driver, self.wait)
+        self.page.go_to_home_page()
+
+    def test_title(self, load_pages):
+        # self.page.check_title("Spotify")
+        self.page.click_on_login()
+
+
+    # def login_spotify(self, load_pages):
