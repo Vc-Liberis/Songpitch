a7a3037189d6d8b08d95a919759e2b4d091c8670
ralftheepic <chauhanvinay19@gmail.com>
2023-12-08 19:32:52 +0530

    resolved conflicts

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,11 @@
+import pandas as pd
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
 class BasePage:
+    excel_file_path = ".\\testData\\SignUpTestData.xlsx"
+    df = pd.read_excel(excel_file_path, sheet_name="Sheet1")
 
     def __init__(self, driver, wait):
         self.driver = driver
@@ -15,13 +18,6 @@ class BasePage:
         return self.driver.title
 
     def click_element(self, element, timeout=10):
-        """
-        Clicks the given WebElement.
-
-        Parameters:
-        - element: WebElement instance
-        - timeout: Maximum time to wait for the element to be clickable (default is 10 seconds)
-        """
         try:
             clickable_element = WebDriverWait(element._parent, timeout).until(
                 EC.element_to_be_clickable(element)
@@ -32,15 +28,6 @@ class BasePage:
             print(f"Failed to click on the element. {str(e)}")
 
     def click_element_by_value(self, driver, by, value, timeout=10):
-        """
-        Clicks an element identified by the given locator strategy (by) and value.
-
-        Parameters:
-        - driver: WebDriver instance
-        - by: The locator strategy (e.g., By.ID, By.XPATH, etc.)
-        - value: The value of the locator
-        - timeout: Maximum time to wait for the element to be clickable (default is 10 seconds)
-        """
         try:
             element = WebDriverWait(driver, timeout).until(
                 EC.element_to_be_clickable((by, value))
@@ -51,61 +38,39 @@ class BasePage:
             print(f"Failed to click on the element. {str(e)}")
 
     def input_text_by_by(self, driver, by, value, text, timeout=10):
-        """
-        Enters the given text into a text box identified by the given locator strategy (by) and value.
-
-        Parameters:
-        - driver: WebDriver instance
-        - by: The locator strategy (e.g., By.ID, By.XPATH, etc.)
-        - value: The value of the locator
-        - text: The text to be entered into the text box
-        - timeout: Maximum time to wait for the text box to be present (default is 10 seconds)
-        """
         try:
             textbox = WebDriverWait(driver, timeout).until(
                 EC.presence_of_element_located((by, value))
             )
-            textbox.clear()  # Clear any existing text in the text box
+            textbox.clear()
             textbox.send_keys(text)
             print(f"Entered '{text}' into the text box successfully.")
         except Exception as e:
             print(f"Failed to enter text into the text box. {str(e)}")
 
     def enter_text_by_locator(self, locator, text):
-        """
-        Enters the given text into a text box identified by the provided locator.
-
-        Parameters:
-        - driver: WebDriver instance
-        - locator: Tuple (By, value) representing the locator strategy and value (e.g., (By.ID, "your_element_id"))
-        - text: The text to be entered into the text box
-        """
         try:
             element = self.wait.until(
                 EC.presence_of_element_located(locator)
             )
-            element.clear()  # Clear any existing text in the text box
+            element.clear()
             element.send_keys(text)
             print(f"Entered '{text}' into the text box successfully.")
         except Exception as e:
             print(f"Failed to enter text into the text box. {str(e)}")
 
-    def scroll_to_element(driver, element):
-        # Scroll until the element is in view
-        driver.execute_script("arguments[0].scrollIntoView(true);", element)
+    def scroll_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", element)
+
+    def read_excel(self):
+        artist_names = self.df['artistName']
+        print(f"Name:::  {artist_names.values}")
+
+    def get_artist_name(self):
+        artist_names_list = self.df['artistName'].tolist()
+        print(f"Name:::  {artist_names_list[0]}")
 
     def wait_for_presence_element(self, locator, timeout=30):
-        """
-        Wait for the element identified by the given locator to be present and visible.
-
-        Parameters:
-        - driver: WebDriver instance
-        - locator: Tuple (By, value) representing the locator strategy and value
-        - timeout: Maximum time to wait for the element (default is 10 seconds)
-
-        Returns:
-        - WebElement once it's present and visible
-        """
         try:
             element = self.wait.until(
                 EC.presence_of_element_located(locator)
@@ -113,18 +78,8 @@ class BasePage:
             return element
         except Exception as e:
             raise TimeoutError(f"Element {locator} not found within {timeout} seconds.") from e
+
     def wait_for_visiblity_element(self, locator, timeout=30):
-        """
-        Wait for the element identified by the given locator to be present and visible.
-
-        Parameters:
-        - driver: WebDriver instance
-        - locator: Tuple (By, value) representing the locator strategy and value
-        - timeout: Maximum time to wait for the element (default is 10 seconds)
-
-        Returns:
-        - WebElement once it's present and visible
-        """
         try:
             element = self.wait.until(
                 EC.visibility_of_element_located(locator)
@@ -134,17 +89,6 @@ class BasePage:
             raise TimeoutError(f"Element {locator} not found within {timeout} seconds.") from e
 
     def wait_for_element(self, locator, timeout=30):
-        """
-        Wait for the element identified by the given locator to be present and visible.
-
-        Parameters:
-        - driver: WebDriver instance
-        - locator: Tuple (By, value) representing the locator strategy and value
-        - timeout: Maximum time to wait for the element (default is 10 seconds)
-
-        Returns:
-        - WebElement once it's present and visible
-        """
         try:
             element = self.wait.until(
                 EC.presence_of_element_located(locator) and EC.visibility_of_element_located(locator)
--- pages/spotify_page.py
+++ pages/spotify_page.py
@@ -15,21 +15,31 @@ class Spotify(BasePage):
     def check_title(self, title):
         self.wait.until(EC.title_contains(title))
 
-    def enter_username(self,username):
-        #self.scroll_to_element(self.locator.EMAIL_USERNAME)
-        self.enter_text_by_locator(self.locator.EMAIL_USERNAME,username)
+    def enter_username(self, username):
+        self.enter_text_by_locator(self.locator.emailUsername, username)
 
-    def enter_password(self,password):
-        #self.scroll_to_element(self.locator.PASSWORD)
-        self.enter_text_by_locator(self.locator.PASSWORD,password)
+    def enter_password(self, password):
+        self.enter_text_by_locator(self.locator.password, password)
 
     def click_on_login(self):
-        self.click_Login_button(*self.locator.LOGIN)
+        self.click_button(*self.locator.loginBtn)
 
-    def click_Login_button(self, by, value):
+    def click_button(self, by, value):
         element = self.wait.until(EC.element_to_be_clickable((by, value)))
         element.click()
 
+    def input_artist_name(self, value):
+        self.enter_text_by_locator(self.locator.searchByArtist, value)
+
+    def click_on_pitchsong_btn(self):
+        self.click_button(*self.locator.pitchASongLinkBtn)
+
+    def click_on_next_btn(self):
+        self.click_button(*self.locator.nextBtn)
+
+    def select_hometown_for_artist(self):
+        self.click_button(*self.locator.hometownForArtists)
+        self.enter_text_by_locator(self.locator.hometownForArtists, "London, England, United Kingdom")
+
     def wait_for_dashboard(self):
         self.wait_for_presence_element(self.locator.ROASTER_ARTIST_TABLE)
-
